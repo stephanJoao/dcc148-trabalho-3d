2428537b0b178347dcb7e3124cf060bdaf2359db
Joao Stephan <joaossmauricio@gmail.com>
2023-12-21 04:47:58 -0300

    procede?

--- Assets/Scripts/MapGeneratorScript.cs
+++ Assets/Scripts/MapGeneratorScript.cs
@@ -5,17 +5,82 @@ using UnityEngine;
 public class MapGeneratorScript : MonoBehaviour
 {
     [SerializeField] int iterations;
-    private int numTiles = 1000;    
+    private int numTiles = 100;    
     private int[,] tiles;
     public GameObject[] rooms;
 
-    void CreateRoom(int i, int j, char type)
+    void CreateRoom(int i, int j, int[] type)
     {
-        
-        GameObject room = Instantiate(rooms[0]);
-        Vector3 pos = new Vector3(i * 150, 0, j * 150);
+        GameObject room;
+
+        if (type[0] == 0 && type[1] == 1 && type[2] == 0 && type[3] == 0)
+        {
+            room = Instantiate(rooms[0]); 
+        }
+        else if (type[0] == 1 && type[1] == 0 && type[2] == 0 && type[3] == 0)
+        {
+            room = Instantiate(rooms[1]);
+        }
+        else if (type[0] == 0 && type[1] == 0 && type[2] == 0 && type[3] == 1)
+        {
+            room = Instantiate(rooms[2]);
+        }
+        else if (type[0] == 0 && type[1] == 0 && type[2] == 1 && type[3] == 0)
+        {
+            room = Instantiate(rooms[3]);
+        }
+        else if (type[0] == 1 && type[1] == 1 && type[2] == 0 && type[3] == 0)
+        {
+            room = Instantiate(rooms[4]);
+        }
+        else if (type[0] == 0 && type[1] == 1 && type[2] == 0 && type[3] == 1)
+        {
+            room = Instantiate(rooms[5]);
+        }
+        else if (type[0] == 0 && type[1] == 1 && type[2] == 1 && type[3] == 0)
+        {
+            room = Instantiate(rooms[6]);
+        }
+        else if (type[0] == 1 && type[1] == 0 && type[2] == 0 && type[3] == 1)
+        {
+            room = Instantiate(rooms[7]);
+        }
+        else if (type[0] == 1 && type[1] == 0 && type[2] == 1 && type[3] == 0)
+        {
+            room = Instantiate(rooms[8]);
+        }
+        else if (type[0] == 0 && type[1] == 0 && type[2] == 1 && type[3] == 1)
+        {
+            room = Instantiate(rooms[9]);
+        }
+        else if (type[0] == 1 && type[1] == 1 && type[2] == 0 && type[3] == 1)
+        {
+            room = Instantiate(rooms[10]);
+        }
+        else if (type[0] == 1 && type[1] == 1 && type[2] == 1 && type[3] == 0)
+        {
+            room = Instantiate(rooms[11]);
+        }
+        else if (type[0] == 0 && type[1] == 1 && type[2] == 1 && type[3] == 1)
+        {
+            room = Instantiate(rooms[12]);
+        }
+        else if (type[0] == 1 && type[1] == 0 && type[2] == 1 && type[3] == 1)
+        {
+            room = Instantiate(rooms[13]);
+        }
+        else if (type[0] == 1 && type[1] == 1 && type[2] == 1 && type[3] == 1)
+        {
+            room = Instantiate(rooms[14]);
+        }
+        else
+        {
+            room = Instantiate(rooms[15]);
+        }
+       
+        Vector3 pos = new Vector3(i * 15, 0, j * 15);
         room.transform.position = pos;
-        room.transform.localScale = new Vector3(10, 10, 10);
+        room.transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
         room.transform.parent = transform;
     }
     
@@ -24,11 +89,11 @@ public class MapGeneratorScript : MonoBehaviour
     void Start()
     {
         // probability of changing direction
-        float p = 0.3f;
+        float p = 0.4f;
         int direction = 0;
         
         // create vector of possible directions: n, s, e, w
-        int[,] directions = { { 0, 1}, { 0, -1}, { 1, 0}, { -1, 0}}; // {x, y}
+        int[,] directions = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}}; // {x, y}
 
         // initialize tiles array with 0s
         tiles = new int[numTiles, numTiles];
@@ -40,10 +105,12 @@ public class MapGeneratorScript : MonoBehaviour
             }
         }
 
+        // start at 0, 0
         int i = 0;
         int j = 0;
-        tiles[i, j] = 1; // start at 0, 0
+        tiles[i, j] = 1; 
 
+        // create path
         while (iterations > 0)
         {
             if(Random.value < p)
@@ -67,17 +134,33 @@ public class MapGeneratorScript : MonoBehaviour
             iterations--;
         }
 
+        // create auxiliary tiles
+        int[,] auxTiles = new int[numTiles+2, numTiles+2];
+        for (int m = 0; m < numTiles+2; m++)
+        {
+            for (int n = 0; n < numTiles+2; n++)
+            {
+                if (m == 0 || n == 0 || m == numTiles+1 || n == numTiles+1)
+                {
+                    auxTiles[m, n] = 0;
+                }
+                else
+                {
+                    auxTiles[m, n] = tiles[m-1, n-1];
+                }
+            }
+        }
+        
         // get all prefabs in Assets/Prefabs/Rooms
         rooms = Resources.LoadAll<GameObject>("Prefabs/Rooms");
 
-        for (int m = 0; m < numTiles; m++)
+        // create rooms
+        for (int m = 1; m < numTiles + 1; m++)
         {
-            for (int n = 0; n < numTiles; n++)
+            for (int n = 1; n < numTiles + 1; n++)
             {
-                if (tiles[m, n] == 1)
-                {
-                    CreateRoom(m, n, 'n');
-                }
+                if (auxTiles[m, n] == 1)
+                    CreateRoom(m, n, new int[] { auxTiles[m+1, n], auxTiles[m, n+1], auxTiles[m-1, n], auxTiles[m, n-1] });
             }
         }        
     }
