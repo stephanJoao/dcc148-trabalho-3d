7779b5287182f5dc472321ab932553c830b07893
aryneto1 <aryneto405@gmail.com>
2023-12-21 13:29:25 -0300

    objeto sound manager com o audio da ambientação

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class SoundManager : MonoBehaviour
 {
+    private AudioSource audioAmbience;
     // Start is called before the first frame update
     void Start()
     {
-        
+        audioAmbience = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
